74641450304c8e75c24b167c7c248ada3128d5fc
umar-qureshi2 <umar.mcs@gmail.com>
2017-09-17 12:49:18 +0400

    added custom map

--- Demos/Demos/Program.cs
+++ Demos/Demos/Program.cs
@@ -1,15 +1,85 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Demos
 {
-    class Program
+    public class Product
     {
-        static void Main(string[] args)
+        public int CategoryID { get; set; }
+        public bool Discontinued { get; set; }
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public short? ReorderLevel { get; set; }
+
+        // reference
+        public Supplier Supplier { get; set; }
+
+        public int SupplierID { get; set; }
+        public decimal UnitPrice { get; set; }
+        public short? UnitsInStock { get; set; }
+        public short? UnitsOnOrder { get; set; }
+    }
+
+    public class Supplier
+    {
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string CompanyName { get; set; }
+        public string ContactName { get; set; }
+        public string ContactTitle { get; set; }
+        public string Country { get; set; }
+        public string PostalCode { get; set; }
+        public List<Product> Products { get; set; }
+        public int SupplierID { get; set; }
+    }
+
+    internal class ProductVM
+    {
+        public string DisplayName { get; set; }
+        public bool Enabled { get; set; }
+        public int Qty { get; set; }
+        public string SupplierName { get; set; }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
         {
+            InitAutoMapper();
+
+            ProductVM productView = new ProductVM()
+            {
+                DisplayName = "Water Glass",
+                Enabled = true,
+                Qty = 1,
+                SupplierName = "Ikea"
+            };
+
+            var product = Mapper.Map<Product>(productView);
+            ObjectDumper.Write(product);
+            Console.WriteLine($"Press any key to continue");
+            Console.ReadKey();
         }
+
+        private static void InitAutoMapper()
+        {
+            Mapper.Initialize(config =>
+            {
+                config.CreateMap<ProductVM, Product>()
+                .ForMember(x => x.ProductName, opt => opt.MapFrom<string>(x => x.DisplayName))
+                .ForMember<bool>(x => x.Discontinued, opt => opt.MapFrom<bool>(x => x.Enabled))
+                .ForMember<short?>(x => x.UnitsInStock, opt => opt.MapFrom<int>(x => x.Qty))
+                .ForMember<Supplier>(x => x.Supplier, opt => opt.MapFrom<Supplier>(x => new Supplier() { CompanyName = x.SupplierName }))
+                ;
+            });
+        }
+    }
+
+    internal class SupplierVM
+    {
+        public int ProductsCount { get; set; }
+        public string SupplierName { get; set; }
     }
 }
